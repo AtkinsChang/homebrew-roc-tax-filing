d517ffd56c7248d1f3498b685705e8650c9fcb87
Atkins <atkins@fstk.io>
2019-05-12 16:16:52 +0800

    Update version

--- Formula/roctaxfiling.rb
+++ Formula/roctaxfiling.rb
@@ -4,14 +4,14 @@ class Roctaxfiling < Formula
   homepage "http://moica.nat.gov.tw/rac_plugin.html"
   url "http://api-hisecurecdn.cdn.hinet.net/HiPKILocalSignServer/mac/HiPKILocalSignServer.pkg"
   mirror "http://moica.nat.gov.tw/download/File/HiPKILocalSignServer/mac/HiPKILocalSignServer.pkg"
-  version "1.3.4.5"
-  sha256 "65b83f2c8f9f165d61ee35c34bac563e86f3178417653b10cc6f5a9dfb84c808"
+  version "1.3.4.7"
+  sha256 "f010bfcb89077ac336034c7114be76f4dac5493d319d047735c4bd6631ea5706"
 
   bottle :unneeded
 
   def install
-    system "tar", "xzf", "HiPKILocalSignServer.pkg/Scripts"
-    system "unzip", "-qq", "HiPKILocalSignServer_install.zip"
+    system "pkgutil", "--expand", "HiPKILocalSignServer.pkg", "expand"
+    system "unzip", "-qq", "expand/HiPKILocalSignServer.pkg/Scripts/HiPKILocalSignServer_install.zip"
 
     lng = `defaults read .GlobalPreferences AppleLanguages`.gsub! /\s/, ""
     inreplace "HiPKILocalSignServer_install/runHIPKILocalServer.sh", "~/Library/HiPKILocalSignServer", libexec
@@ -23,6 +23,8 @@ class Roctaxfiling < Formula
     else
       libexec.install Dir["HiPKILocalSignServer_install/HiPKILocalSignServer_zhTW/*"]
     end
+    bin.install "HiPKILocalSignServer_install/checkHIPKILocalServer.sh"
+    bin.install "HiPKILocalSignServer_install/stopHIPKILocalServer.sh"
     bin.install "HiPKILocalSignServer_install/runHIPKILocalServer.sh"
   end
 end
